b068f03af6eedbd7fb91112f378d7dcd71e6a278
danielRamos <daniel.r.c.dk23@gmail.com>
2026-07-13 16:03:04 -0600

    login v1

---
+++ Sistema_Ventas/Frm_Login.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Sistema_Ventas
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_contraseña = new System.Windows.Forms.TextBox();
+            this.btn_iniciar_sesión = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Sistema_Ventas.Properties.Resources.iconopro;
+            this.pictureBox1.Location = new System.Drawing.Point(353, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_usuario.Location = new System.Drawing.Point(353, 257);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(187, 26);
+            this.txt_usuario.TabIndex = 1;
+            this.txt_usuario.Text = "👤   Usuario";
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_contraseña.Location = new System.Drawing.Point(353, 301);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(187, 26);
+            this.txt_contraseña.TabIndex = 2;
+            this.txt_contraseña.Text = "🔒   Contraseña";
+            // 
+            // btn_iniciar_sesión
+            // 
+            this.btn_iniciar_sesión.BackgroundImage = global::Sistema_Ventas.Properties.Resources.fondoiniciarsesion;
+            this.btn_iniciar_sesión.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_iniciar_sesión.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_iniciar_sesión.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_iniciar_sesión.Location = new System.Drawing.Point(353, 355);
+            this.btn_iniciar_sesión.Name = "btn_iniciar_sesión";
+            this.btn_iniciar_sesión.Size = new System.Drawing.Size(187, 37);
+            this.btn_iniciar_sesión.TabIndex = 3;
+            this.btn_iniciar_sesión.UseVisualStyleBackColor = true;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sistema_Ventas.Properties.Resources.Fondo_login;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(887, 495);
+            this.Controls.Add(this.btn_iniciar_sesión);
+            this.Controls.Add(this.txt_contraseña);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_contraseña;
+        private System.Windows.Forms.Button btn_iniciar_sesión;
+    }
+}
