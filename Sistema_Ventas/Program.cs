7b300b097979940d0fc1be9e1edc39c9909b3514
danielRamos <daniel.r.c.dk23@gmail.com>
2026-07-16 22:47:32 -0600

    correcion program

--- Sistema_Ventas/Program.cs
+++ Sistema_Ventas/Program.cs
@@ -17,6 +17,7 @@ namespace Sistema_Ventas
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
