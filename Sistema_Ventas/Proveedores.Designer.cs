e2dbd6b8e211f5c5ae17ecd259d9fda81c7a50f6
Ariadna Yenisey <yeniseyflores718@gamil.com>
2026-07-19 19:36:32 -0600

    ajustes a proveedores

--- Sistema_Ventas/Proveedores.Designer.cs
+++ Sistema_Ventas/Proveedores.Designer.cs
@@ -83,9 +83,17 @@ namespace Sistema_Ventas
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_proveedores)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
             this.tableLayoutPanel4.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel9.SuspendLayout();
             this.tableLayoutPanel5.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel12.SuspendLayout();
             this.panel2.SuspendLayout();
             this.tableLayoutPanel6.SuspendLayout();
             this.panel4.SuspendLayout();
@@ -255,7 +263,7 @@ namespace Sistema_Ventas
             this.btn_actualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_actualizar.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_actualizar.ForeColor = System.Drawing.Color.White;
-            this.btn_actualizar.Location = new System.Drawing.Point(3, 13);
+            this.btn_actualizar.Location = new System.Drawing.Point(10, 9);
             this.btn_actualizar.Name = "btn_actualizar";
             this.btn_actualizar.Size = new System.Drawing.Size(127, 28);
             this.btn_actualizar.TabIndex = 38;
@@ -269,7 +277,7 @@ namespace Sistema_Ventas
             this.btn_eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_eliminar.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_eliminar.ForeColor = System.Drawing.Color.White;
-            this.btn_eliminar.Location = new System.Drawing.Point(9, 13);
+            this.btn_eliminar.Location = new System.Drawing.Point(9, 9);
             this.btn_eliminar.Name = "btn_eliminar";
             this.btn_eliminar.Size = new System.Drawing.Size(127, 28);
             this.btn_eliminar.TabIndex = 37;
@@ -283,7 +291,7 @@ namespace Sistema_Ventas
             this.btn_agregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_agregar.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_agregar.ForeColor = System.Drawing.Color.White;
-            this.btn_agregar.Location = new System.Drawing.Point(19, 13);
+            this.btn_agregar.Location = new System.Drawing.Point(11, 6);
             this.btn_agregar.Name = "btn_agregar";
             this.btn_agregar.Size = new System.Drawing.Size(127, 28);
             this.btn_agregar.TabIndex = 36;
@@ -294,7 +302,7 @@ namespace Sistema_Ventas
             // 
             this.txt_diaentrega.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txt_diaentrega.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_diaentrega.Location = new System.Drawing.Point(95, 11);
+            this.txt_diaentrega.Location = new System.Drawing.Point(95, 12);
             this.txt_diaentrega.Multiline = true;
             this.txt_diaentrega.Name = "txt_diaentrega";
             this.txt_diaentrega.Size = new System.Drawing.Size(127, 16);
@@ -305,7 +313,7 @@ namespace Sistema_Ventas
             // 
             this.txt_estado.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txt_estado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_estado.Location = new System.Drawing.Point(53, 9);
+            this.txt_estado.Location = new System.Drawing.Point(86, 14);
             this.txt_estado.Multiline = true;
             this.txt_estado.Name = "txt_estado";
             this.txt_estado.Size = new System.Drawing.Size(273, 16);
@@ -315,7 +323,7 @@ namespace Sistema_Ventas
             // 
             this.txt_empresa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txt_empresa.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_empresa.Location = new System.Drawing.Point(67, 11);
+            this.txt_empresa.Location = new System.Drawing.Point(68, 12);
             this.txt_empresa.Multiline = true;
             this.txt_empresa.Name = "txt_empresa";
             this.txt_empresa.Size = new System.Drawing.Size(144, 16);
@@ -325,7 +333,7 @@ namespace Sistema_Ventas
             // 
             this.txt_telefono.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.txt_telefono.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_telefono.Location = new System.Drawing.Point(60, 9);
+            this.txt_telefono.Location = new System.Drawing.Point(77, 14);
             this.txt_telefono.Multiline = true;
             this.txt_telefono.Name = "txt_telefono";
             this.txt_telefono.Size = new System.Drawing.Size(273, 16);
@@ -335,7 +343,7 @@ namespace Sistema_Ventas
             // 
             this.txt_nombre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txt_nombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_nombre.Location = new System.Drawing.Point(64, 11);
+            this.txt_nombre.Location = new System.Drawing.Point(63, 14);
             this.txt_nombre.Multiline = true;
             this.txt_nombre.Name = "txt_nombre";
             this.txt_nombre.Size = new System.Drawing.Size(238, 16);
@@ -347,7 +355,7 @@ namespace Sistema_Ventas
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(3, 11);
+            this.label6.Location = new System.Drawing.Point(3, 12);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(86, 14);
             this.label6.TabIndex = 24;
@@ -360,7 +368,7 @@ namespace Sistema_Ventas
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(3, 11);
+            this.label5.Location = new System.Drawing.Point(26, 14);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(44, 14);
             this.label5.TabIndex = 23;
@@ -372,7 +380,7 @@ namespace Sistema_Ventas
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(5, 11);
+            this.label4.Location = new System.Drawing.Point(3, 12);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 14);
             this.label4.TabIndex = 22;
@@ -384,7 +392,7 @@ namespace Sistema_Ventas
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 11);
+            this.label3.Location = new System.Drawing.Point(16, 14);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 14);
             this.label3.TabIndex = 21;
@@ -396,7 +404,7 @@ namespace Sistema_Ventas
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(7, 11);
+            this.label2.Location = new System.Drawing.Point(3, 14);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 14);
             this.label2.TabIndex = 20;
@@ -418,7 +426,7 @@ namespace Sistema_Ventas
             this.dgv_proveedores.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgv_proveedores.Location = new System.Drawing.Point(0, 0);
             this.dgv_proveedores.Name = "dgv_proveedores";
-            this.dgv_proveedores.Size = new System.Drawing.Size(149, 47);
+            this.dgv_proveedores.Size = new System.Drawing.Size(778, 208);
             this.dgv_proveedores.TabIndex = 19;
             this.dgv_proveedores.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_proveedores_CellContentClick);
             // 
@@ -492,6 +500,7 @@ namespace Sistema_Ventas
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.dgv_proveedores);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(3, 88);
             this.panel3.Name = "panel3";
@@ -517,6 +526,8 @@ namespace Sistema_Ventas
             // 
             // panel5
             // 
+            this.panel5.Controls.Add(this.txt_nombre);
+            this.panel5.Controls.Add(this.label2);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel5.Location = new System.Drawing.Point(3, 3);
             this.panel5.Name = "panel5";
@@ -525,6 +536,8 @@ namespace Sistema_Ventas
             // 
             // panel6
             // 
+            this.panel6.Controls.Add(this.txt_empresa);
+            this.panel6.Controls.Add(this.label4);
             this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel6.Location = new System.Drawing.Point(314, 3);
             this.panel6.Name = "panel6";
@@ -533,6 +546,8 @@ namespace Sistema_Ventas
             // 
             // panel7
             // 
+            this.panel7.Controls.Add(this.txt_diaentrega);
+            this.panel7.Controls.Add(this.label6);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel7.Location = new System.Drawing.Point(547, 3);
             this.panel7.Name = "panel7";
@@ -556,6 +571,8 @@ namespace Sistema_Ventas
             // 
             // panel8
             // 
+            this.panel8.Controls.Add(this.txt_telefono);
+            this.panel8.Controls.Add(this.label3);
             this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel8.Location = new System.Drawing.Point(3, 3);
             this.panel8.Name = "panel8";
@@ -564,6 +581,8 @@ namespace Sistema_Ventas
             // 
             // panel9
             // 
+            this.panel9.Controls.Add(this.txt_estado);
+            this.panel9.Controls.Add(this.label5);
             this.panel9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel9.Location = new System.Drawing.Point(392, 3);
             this.panel9.Name = "panel9";
@@ -593,6 +612,7 @@ namespace Sistema_Ventas
             // 
             // panel11
             // 
+            this.panel11.Controls.Add(this.btn_agregar);
             this.panel11.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel11.Location = new System.Drawing.Point(158, 3);
             this.panel11.Name = "panel11";
@@ -601,6 +621,7 @@ namespace Sistema_Ventas
             // 
             // panel12
             // 
+            this.panel12.Controls.Add(this.btn_eliminar);
             this.panel12.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel12.Location = new System.Drawing.Point(313, 3);
             this.panel12.Name = "panel12";
@@ -610,19 +631,6 @@ namespace Sistema_Ventas
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(244)))), ((int)(((byte)(250)))));
-            this.panel2.Controls.Add(this.btn_eliminar);
-            this.panel2.Controls.Add(this.btn_agregar);
-            this.panel2.Controls.Add(this.txt_diaentrega);
-            this.panel2.Controls.Add(this.txt_estado);
-            this.panel2.Controls.Add(this.txt_empresa);
-            this.panel2.Controls.Add(this.txt_telefono);
-            this.panel2.Controls.Add(this.txt_nombre);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.dgv_proveedores);
             this.panel2.Controls.Add(this.btn_actualizar);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(468, 3);
@@ -698,11 +706,23 @@ namespace Sistema_Ventas
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_proveedores)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
             this.tableLayoutPanel4.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
             this.tableLayoutPanel5.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel12.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.tableLayoutPanel6.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
